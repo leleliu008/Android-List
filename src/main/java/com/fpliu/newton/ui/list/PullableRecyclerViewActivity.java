07da9b6210918bde7452d7270bf63ec8fd3b1abb
leleliu008 <792793182@qq.com>
2017-07-25 17:47:32 +0800

    optimization

--- src/main/java/com/fpliu/newton/ui/list/PullableRecyclerViewActivity.java
+++ src/main/java/com/fpliu/newton/ui/list/PullableRecyclerViewActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import com.fpliu.newton.ui.base.BaseActivity;
 import com.fpliu.newton.ui.pullable.PullType;
@@ -24,7 +23,7 @@ import java.util.List;
  * @author 792793182@qq.com 2016-06-06.
  */
 public abstract class PullableRecyclerViewActivity<T, H extends ItemViewHolderAbs> extends BaseActivity
-        implements IPullableRecyclerView<T, H>, OnItemClickListener<T, H>, RefreshOrLoadMoreCallback<ListView> {
+        implements IPullableRecyclerView<T, H>, OnItemClickListener<T, H>, RefreshOrLoadMoreCallback<RecyclerView> {
 
     private IPullableRecyclerView<T, H> pullableRecyclerView;
 
--- src/main/java/com/fpliu/newton/ui/list/PullableRecyclerViewFragment.java
+++ src/main/java/com/fpliu/newton/ui/list/PullableRecyclerViewFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import com.fpliu.newton.ui.base.BaseView;
 import com.fpliu.newton.ui.base.LazyFragment;
@@ -24,7 +23,7 @@ import java.util.List;
  * @author 792793182@qq.com 2016-06-06.
  */
 public abstract class PullableRecyclerViewFragment<T, H extends ItemViewHolderAbs> extends LazyFragment implements
-        IPullableRecyclerView<T, H>, OnItemClickListener<T, H>, RefreshOrLoadMoreCallback<ListView> {
+        IPullableRecyclerView<T, H>, OnItemClickListener<T, H>, RefreshOrLoadMoreCallback<RecyclerView> {
 
     private IPullableRecyclerView<T, H> pullableRecyclerView;
 
